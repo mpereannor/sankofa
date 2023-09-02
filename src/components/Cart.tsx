02e0179bc0e14325e525c9dbf3a6518def6c7cee
mpere <mpereannor@gmail.com>
2023-09-02 02:09:27 +0000

    responsive cart v2

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -16,7 +16,7 @@ function Cart({
       <AnimatePresence>
         {cartOpen && (
           <motion.aside
-            className=" fixed top-20 bottom-0 bg-white  right-0 h-fit z-[999] [--width:90%] lg:[--width:50%]"
+            className=" fixed top-20 bottom-0 bg-white  right-0 h-fit z-[999] [--width:90%] md:[--width:60%] lg:[--width:50%]"
             initial={{ width: 0 }}
             animate={{
               width: "var(--width)",
@@ -31,26 +31,27 @@ function Cart({
                   className="rounded-2xl bg-black p-1.5"
                 />
               </div>
-              <div className="relative h-full mx-auto justify-between flex flex-col  m-4">
+              <div className="relative h-full mx-auto justify-between flex flex-col  sm:bg-transparent  m-4">
                 <div className="">
                   <div className="">
-                    <div className=" mb-2 p-4 border-gray-300 border-b flex">
+                    <div className=" mb-2 sm:py-4 sm:px-4 py:2 px:2 border-gray-300 border-b flex    ">
                       <img
                         src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                         alt="product-image"
-                        className="  w-20"
+                        className=" sm:block hidden  w-20"
                       />
-                      <div className=" ml-4 flex w-full justify-between">
-                        <div className="mt-5">
+                      <div className=" ml-4 flex w-full justify-between flex-col sm:flex-row sm:gap-y-0 gap-y-6">
+                        <div className="mt-5  w-full sm:w-1/3">
                           <h2 className="text-md font-semibold text-gray-900">
                             Nike Air Max 2019
                           </h2>
                           <p className="mt-1 text-xs font-light italic text-gray-700">
-                            Color impasto on board, gold leaf
+                            Color impasto on board, gold leaf Color impasto on
+                            board, gold leaf
                           </p>
                         </div>
-                        <div className=" flex justify-between space-y-6 sm:block space-x-6">
-                          <div className="flex items-center justify-center">
+                        <div className=" flex flex-col sm:flex-row justify-between space-y-0 sm:space-y-6 sm:block space-x-6 w-full sm:w-2/3 sm:gap-y-0 gap-y-6">
+                          <div className="flex items-center sm:justify-center ">
                             <span className="cursor-pointer mx-2 bg-black py-1 px-3.5 duration-100 hover:bg-gray-800 hover:text-blue-50">
                               {" "}
                               -{" "}
@@ -66,7 +67,7 @@ function Cart({
                               +{" "}
                             </span>
                           </div>
-                          <div className="flex items-center space-x-4">
+                          <div className="flex sm:items-center items:left space-x-4 py-4 sm:py-0">
                             <p className="text-sm text-black font-light">
                               GHS 250, 000. 00
                             </p>
@@ -88,7 +89,7 @@ function Cart({
                         </div>
                       </div>
                     </div>
-                    <div className=" mb-2 p-4 border-gray-300 border-b flex">
+                    {/* <div className=" mb-2 p-4 border-gray-300 border-b flex">
                       <img
                         src="https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80"
                         alt="product-image"
@@ -141,7 +142,7 @@ function Cart({
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
                 <div className="">
